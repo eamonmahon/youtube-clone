cf34f0784a6fab1c2a4a4b2cee776ebe7b474899
Eamon Mahon <eamon.mahon@ucd.ie>
2024-09-27 18:09:23 +0100

    process videos locally

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,36 @@
 import express from "express";
+// wrapper around CLI tool; won't do anything if CLI tool not installed; way of using it inside code
+import ffmpeg from "fluent-ffmpeg";
 
 // create express app
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-// define route handler for GET request at the root URL
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+// define end point for GET request at the root URL
+app.post("/process-video", (req, res) => {
+    // Get path of the input video file from the request body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing File Path");
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions('-vf', 'scale=-1:360') // convert video to 360p
+        .on("end", () => {
+            res.status(200).send("Processing finished successfully.");
+        })
+        .on("error", (err) => {
+            console.log(`An error occurred: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`);
+        })
+        .save(outputFilePath);
+    
 });
+
+// provide port at runtime; possible that the env var may not be defined in which case its 3000
+const port = process.env.PORT || 3000;
 
 // start server, listen for incoming connections on specified port
 app.listen(port, () => {
